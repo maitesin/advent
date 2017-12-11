966bbca8654520d053dd7681c0fb1164bceb41be
Oscar Forner Martinez <oscar.forner@vcatechnology.com>
2017-12-11 10:22:40 +0000

    Add code to solve day eleven part two

---
+++ 11/part_2/code.py
@@ -0,0 +1,40 @@
+import sys
+import math
+
+def main(line):
+    steps = [x for x in line.split(',')]
+    x = 0
+    y = 0
+    max = 0
+    for step in steps:
+        if len(step) == 2:
+            if 'n' in step:
+                y = y + 1
+            if 's' in step:
+                y = y - 1
+            if 'e' in step:
+                x = x + 1
+            if 'w' in step:
+                x = x - 1
+        else:
+            if 'n' == step:
+                y = y + 2
+            else:
+                y = y - 2
+        n_max = distance(x, y)
+        if n_max > max:
+            max = n_max
+    print(max)
+
+def distance(x, y):
+    ax = abs(x)
+    ay = abs(y)
+    if ax < ay:
+        dif = ay - ax
+        return ax + math.floor(dif/2)
+    else:
+        return ax
+
+
+if __name__ == "__main__":
+    main(open(sys.argv[1], 'r').readline().strip())
