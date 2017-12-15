6349c42154a4d65baa760920198f7b17932d29d7
Oscar Forner Martinez <oscar.forner@vcatechnology.com>
2017-12-15 11:22:22 +0000

    Add code to solve day fifteen part one

---
+++ 15/part_1/code.py
@@ -0,0 +1,45 @@
+import sys
+
+class Generator(object):
+    def __init__(self, value, factor):
+        self.value = value
+        self.factor = factor
+        self.div = 2147483647
+
+    def next(self):
+        new = int((self.value * self.factor) % self.div)
+        self.value = new
+        return new
+
+def main(lines):
+    init_a = get_input_value(lines[0])
+    init_b = get_input_value(lines[1])
+    gen_a = Generator(init_a, 16807)
+    gen_b = Generator(init_b, 48271)
+    count = 0
+    for i in range(40000000):
+        bits_a = get_bits(gen_a.next())
+        bits_b = get_bits(gen_b.next())
+        if bits_a == bits_b:
+            count += 1
+    print(count)
+
+def get_input_value(line):
+    return int(line.split(' ')[4].strip())
+
+def get_bits(value):
+    bits = ""
+    index = 0
+    while index < 16:
+        bits = str(value % 2) + bits
+        value = value >> 1
+        index += 1
+        if value == 0:
+            break
+    while index < 16:
+        bits = '0' + bits
+        index += 1
+    return bits
+
+if __name__ == '__main__':
+    main(open(sys.argv[1], 'r').readlines())
