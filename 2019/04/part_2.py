4a086d5bd3267318a355d3eed37c01482718edd4
Oscar Forner Martinez <oscar.forner@globalsign.com>
2019-12-04 15:59:31 +0000

    Add solution for day 4

--- 2019/04/part_2.py
+++ 2019/04/part_2.py
@@ -1,11 +1,20 @@
 import sys
 
-def has_double_digits(value):
+def get_double_digits(value):
     found = False
+    doubles = []
     for pos in range(len(value) - 1):
         if value[pos] == value[pos + 1]:
-            found = True
-    return found
+            doubles.append(value[pos])
+    return set(doubles)
+
+def get_triple_digits(value):
+    found = False
+    triplets = []
+    for pos in range(len(value) - 2):
+        if value[pos] == value[pos + 1] and value[pos] == value[pos + 2]:
+            triplets.append(value[pos])
+    return set(triplets)
 
 def has_six_digits(value):
     return len(value) == 6
@@ -19,8 +28,11 @@ def has_increasing_values(value):
 def valid_passwords_generator(lower, upper):
     for value in range(lower, upper + 1):
         value_str = str(value)
-        if has_six_digits(value_str) and has_double_digits(value_str) and has_increasing_values(value_str):
-            yield value
+        if has_six_digits(value_str) and has_increasing_values(value_str):
+            doubles = get_double_digits(value_str)
+            triplets = get_triple_digits(value_str)
+            if len(doubles) > len(triplets):
+                yield value
 
 def count_number_of_passwords(lower, upper):
     number_of_passwords = 0
