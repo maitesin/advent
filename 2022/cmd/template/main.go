8f04c0448a6467ed6055ca3f0c821abfe7e22e0e
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-11-25 21:04:30 +0100

    Move the input of the template to their own files

--- 2022/cmd/template/main.go
+++ 2022/cmd/template/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"io"
+	"log"
 	"strings"
 )
 
-func main() {
-	input1 := ``
-	fmt.Printf("Task1: %q\n", Task1(io.NopCloser(strings.NewReader(input1))))
+//go:embed inputs/*.txt
+var inputsFS embed.FS
 
-	input2 := ``
-	fmt.Printf("Task2: %q\n", Task2(io.NopCloser(strings.NewReader(input2))))
+func main() {
+	input1, err := inputsFS.ReadFile("inputs/1.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("Task1: %q\n", Task1(io.NopCloser(strings.NewReader(string(input1)))))
+
+	input2, err := inputsFS.ReadFile("inputs/2.txt")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("Task2: %q\n", Task2(io.NopCloser(strings.NewReader(string(input2)))))
 }
 
 func Task1(rc io.ReadCloser) string {
