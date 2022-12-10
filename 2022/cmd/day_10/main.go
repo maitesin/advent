0e8428abbce6544ac60b6eb081d77f269ca1421d
Oscar Forner Martinez <oscar.forner.martinez@gmail.com>
2022-12-10 13:24:31 +0100

    Add Day 10 rewritten

--- 2022/cmd/day_10/main.go
+++ 2022/cmd/day_10/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"embed"
 	"fmt"
+	"github.com/maitesin/advent/2022/internal/cpu"
 	"log"
-	"strconv"
 	"strings"
 )
 
@@ -22,117 +22,61 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Printf("Task2: %q\n", Task2(string(input2)))
+	fmt.Println("Task2:")
+	fmt.Print(Task2(string(input2)))
 }
 
 func Task1(input string) string {
-	instructions := strings.Split(strings.Trim(input, "\n"), "\n")
-	cycle := 1
-	x := 1
-	prevX := x
-
-	stops := []int{20, 60, 100, 140, 180, 220}
 	accum := 0
-	sI := 0
+	n := 0
+	c := cpu.NewCPU()
+	instructions := strings.Split(strings.Trim(input, "\n"), "\n")
 
-	for _, instruction := range instructions {
-		parts := strings.Split(instruction, " ")
-		oper := parts[0]
-		value := 0
-		var err error
-		if len(parts) > 1 {
-			value, err = strconv.Atoi(parts[1])
+	for i := 0; i <= 220; i++ {
+		switch i {
+		case 20, 60, 100, 140, 180, 220:
+			accum += i * c.X()
+		}
+		if c.Tick() {
+			ins, err := cpu.NewInstruction(instructions[n])
 			if err != nil {
 				log.Panic(err)
 			}
-		}
 
-		switch oper {
-		case "addx":
-			cycle += 2
-			x += value
-		case "noop":
-			cycle++
-		default:
-			log.Panic("You should not see this")
+			c.Compute(ins)
+			n++
 		}
-
-		if sI < len(stops) {
-			switch {
-			case cycle == stops[sI]:
-				accum += x * stops[sI]
-				sI++
-			case cycle > stops[sI]:
-				accum += prevX * stops[sI]
-				sI++
-			}
-		} else {
-			break
-		}
-
-		prevX = x
 	}
 
 	return fmt.Sprint(accum)
 }
 
 func Task2(input string) string {
+	screen := make([]string, 40*6)
+	c := cpu.NewCPU()
 	instructions := strings.Split(strings.Trim(input, "\n"), "\n")
 	n := 0
-	cycle := 1
-	x := 1
-	prevX := x
-	wait := 0
 
-	screen := make([]string, 40*6)
-
-	for {
-		if wait == 0 {
-			prevX = x
-			if n >= len(instructions) {
-				break
-			}
-			parts := strings.Split(instructions[n], " ")
-			oper := parts[0]
-			value := 0
-			var err error
-			if len(parts) > 1 {
-				value, err = strconv.Atoi(parts[1])
-				if err != nil {
-					log.Panic(err)
-				}
+	for i := 0; i < len(screen); i++ {
+		if c.Tick() {
+			ins, err := cpu.NewInstruction(instructions[n])
+			if err != nil {
+				log.Panic(err)
 			}
 
-			switch oper {
-			case "addx":
-				wait += 2
-				x += value
-			case "noop":
-				wait++
-			default:
-				log.Panic("You should not see this")
-			}
-
+			c.Compute(ins)
 			n++
-		} else {
-			wait--
 		}
-
-		cPos := ((cycle - 1) % 240) % 40
-		if cPos+1 >= prevX && cPos-1 <= prevX {
-			screen[(cycle-1)%240] = "#"
+		if c.X()-1 <= i%40 && i%40 <= c.X()+1 {
+			screen[i] = "#"
 		} else {
-			screen[(cycle-1)%240] = "."
+			screen[i] = "."
 		}
-
-		cycle++
 	}
-
-	fmt.Println(screen)
 
 	output := ""
 	for i := 0; i < 6; i++ {
 		output += fmt.Sprintln(strings.Join(screen[i*40:(i+1)*40], ""))
 	}
-	return output
+	return fmt.Sprint(output)
 }
--- 2022/internal/cpu/cpu.go
+++ 2022/internal/cpu/cpu.go
@@ -1,1 +1,69 @@
 package cpu
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Instruction interface {
+	Compute(*CPU)
+}
+
+func NewInstruction(code string) (Instruction, error) {
+	parts := strings.Split(code, " ")
+
+	switch len(parts) {
+	case 1:
+		return &Noop{}, nil
+	case 2:
+		value, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return nil, err
+		}
+		return &AddX{x: value}, nil
+	default:
+		return nil, fmt.Errorf("instruction %q not recognized", code)
+	}
+}
+
+type Noop struct{}
+
+func (n *Noop) Compute(cpu *CPU) {
+}
+
+type AddX struct {
+	x int
+}
+
+func (a *AddX) Compute(cpu *CPU) {
+	cpu.wait = 1
+	cpu.nextX = cpu.x + a.x
+}
+
+type CPU struct {
+	x     int
+	nextX int
+	wait  int
+}
+
+func NewCPU() *CPU {
+	return &CPU{nextX: 1}
+}
+
+func (c *CPU) Tick() bool {
+	if c.wait == 0 {
+		c.x = c.nextX
+		return true
+	}
+	c.wait--
+	return false
+}
+
+func (c *CPU) Compute(instruction Instruction) {
+	instruction.Compute(c)
+}
+
+func (c *CPU) X() int {
+	return c.x
+}
